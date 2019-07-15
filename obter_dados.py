668b9133227fe12ad865d8ed87477a615ebfbbcb
Sidney Ramos <rramos.sidney@gmail.com>
2019-07-15 12:22:27 -0400

    Update Script para obtenção de dados

--- obter_dados.py
+++ obter_dados.py
@@ -1,42 +1,49 @@
+#!/usr/bin/python3
 import sys
 import json
 import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-# Autenticação com a API Spotify
-client_credentials_manager = SpotifyClientCredentials(client_id = 'CLIENT_ID',
-                                                      client_secret='CLIENT_SECRET')
-# Objeto Spotipy
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+def autentica(client_id, client_secret):
+    client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)    
+    return spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-# Argumento
-while True:
-    try:
-        uri = sys.argv[1]
-    except:
-        print('Informe a URI')
-        sys.exit()
-        
-# Obtendo nome do artista
-artist = sp.artist(uri)
-artist = artist['name'].replace(' ','_')
+def nome_artista(sp, uri_artista):
+    artist = sp.artist(uri)
+    return artist['name'].replace(' ','_')
 
-# Obtendo melhores tracks
-top_tracks = sp.artist_top_tracks(uri)
+def features_top_tracks(sp, uri_artista):
+    top_tracks = sp.artist_top_tracks(uri)
+    uri_tracks = []
+    for track in top_tracks['tracks']:
+        uri_tracks.append(track['uri'])
+    return sp.audio_features(uri_tracks)
 
-# Obtendo 'uri' das melhores tracks
-uri_tracks = []
-for track in top_tracks['tracks']:
-    uri_tracks.append(track['uri'])
+def converte_json(features, artist):
+    with open('features_' + artist.lower() + '.json','w') as fp:
+        json.dump(features, fp)
+    print('Features convertido para json.')
 
-# Obtendo 'features'
-features = sp.audio_features(uri_tracks)
+def converte_csv(features, artist):
+    df = pd.DataFrame(features)
+    df.to_csv('features_' + artist.lower() + '.csv', index=False)
+    print('Features convertido para csv.')
 
-# Convertendo para JSON
-with open('features_' + artist.lower() + '.json','w') as fp:
-    json.dump(features, fp)
-
-# Convertendo para CSV
-df = pd.DataFrame(features)
-df.to_csv('features_' + artist.lower() + '.csv', index=False)
+if __name__ == "__main__":
+    while True:
+        try:
+            uri = sys.argv[1]
+            spotify = autentica('fe87f2441ae24a409617ab297dfc8579',
+                                '60e422252f744b02b2919bdd44f88245')
+            artista = nome_artista(spotify, uri)
+            features = features_top_tracks(spotify, uri)
+            converte_csv(features, artista)
+            converte_json(features, artista) 
+            break   
+        except spotipy.oauth2.SpotifyOauthError: 
+            print('Informe o Client ID & Client Secret.')
+            sys.exit()
+        except IndexError: 
+            print('Informe a URI do Artista.')
+            sys.exit()
